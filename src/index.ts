b7909192e848c2e86761338d8633d2c6071ec9e3
Tomek Banach <tomek@banciur.org>
2017-11-22 02:50:06 +0100

    Add config with wallet addresses

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+import * as c from "../config.json";
+import {IConfig} from "./typings/config";
+
+// TODO: this should be done properly
+const config = c as IConfig;
+
+console.log(config.wallets);
---
+++ src/typings/config.d.ts
@@ -0,0 +1,3 @@
+export interface IConfig {
+    wallets: [string];
+}
