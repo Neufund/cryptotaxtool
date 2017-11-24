d1f01641dff18ce64daef03faacd731f3c31fefc
Tomek Banach <tomek@banciur.org>
2017-11-24 05:20:21 +0100

    Add csv dump

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { writeFile} from "fs";
+import * as json2csv from "json2csv";
+
 import * as c from "../config.json";
 import { getTransactions } from "./transactions";
 import {IConfig} from "./typings/config";
@@ -7,7 +10,22 @@ const config = c as IConfig;
 
 const code = async () =>  {
     const txs = await getTransactions(config.wallets);
-    console.log(txs);
+    // console.log(txs);
+
+    writeToFile(txs);
+
 };
 
 code();
+
+const writeToFile = (transactions: any) => {
+    // const fields = ["gasPrice", "gasUsed", "hash", "isError", "time", "to", "value"];
+    const csv = json2csv({ data: transactions, fields: Object.keys(transactions[0])});
+
+    writeFile("file.csv", csv, (err) => {
+        if (err){
+            throw err;
+        }
+        console.log("file saved");
+    });
+};
