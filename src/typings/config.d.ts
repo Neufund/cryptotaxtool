cddd597c1395f0f81b46ff677e32d1630e261356
Tomek Banach <tomek@banciur.org>
2018-03-16 20:29:30 +0100

    Reformat config.d.ts

--- src/typings/config.d.ts
+++ src/typings/config.d.ts
@@ -3,28 +3,28 @@ import * as Moment from "moment";
 import { FiatCurrency } from "../constants";
 
 export interface IConfig {
-    startDate: Moment.Moment;
-    endDate: Moment.Moment;
-    ethScanApiKey: string;
-    wallets: Array<{
-        address: string;
-        alias: string;
-        isDev?: boolean;
-    }>;
-    contracts: Array<{
-        address: string;
-        alias: string;
-    }>;
-    fiatCurrency: FiatCurrency;
-    kraken: {
-        enabled: boolean,
-        key: string,
-        secret: string,
-    };
-    email: {
-        enabled: boolean,
-        destEmail: string,
-        userName: string,
-        appPassword: string,
-    };
+  startDate: Moment.Moment;
+  endDate: Moment.Moment;
+  ethScanApiKey: string;
+  wallets: Array<{
+    address: string;
+    alias: string;
+    isDev?: boolean;
+  }>;
+  contracts: Array<{
+    address: string;
+    alias: string;
+  }>;
+  fiatCurrency: FiatCurrency;
+  kraken: {
+    enabled: boolean;
+    key: string;
+    secret: string;
+  };
+  email: {
+    enabled: boolean;
+    destEmail: string;
+    userName: string;
+    appPassword: string;
+  };
 }
