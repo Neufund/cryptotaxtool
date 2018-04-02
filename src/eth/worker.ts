0de023989f540ff617a2be783d6dddd48b4ff3f8
Tomek Banach <tomek@banciur.org>
2018-04-03 00:47:33 +0200

    Add TODO comment to ETH scanning

--- src/eth/worker.ts
+++ src/eth/worker.ts
@@ -5,6 +5,7 @@ import { getTransactions, parseTransactions } from "./etherscan";
 
 export const gatherETH = async (): Promise<ILedgerEntry[]> => {
   const walletsToCheck = config.ETH.wallets.map(wallet => wallet.address);
+  // TODO: we need functionality to log contracts created that not on config contract list
   const rawEthTxs = await getTransactions(walletsToCheck);
   const parsedTxs = parseTransactions(rawEthTxs);
   return computeTransactions(parsedTxs);
