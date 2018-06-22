10f261d064ee75d479d3f1659a170e5a714734c8
Tomek Banach <tomek@banciur.org>
2018-06-23 00:14:09 +0200

    Rewrite cointracking info importer

--- src/cointracking/worker.ts
+++ src/cointracking/worker.ts
@@ -3,7 +3,8 @@ import { readFileSync } from "fs";
 import * as Moment from "moment";
 import * as Papa from "papaparse";
 
-import { ILedgerEntry, TxType } from "../typings/types";
+import { config } from "../config";
+import { CryptoCurrency, ExpenseType, ILedgerEntry, TxType } from "../typings/types";
 import { parseCurrency } from "../utils";
 
 const COIN_TRACKING_DATE_FORMAT = "DD.MM.YYYY HH:mm";
@@ -23,7 +24,7 @@ const COIN_TRACKING_DATE_FORMAT = "DD.MM.YYYY HH:mm";
  */
 
 export const coinTrackingImport = (): ILedgerEntry[] => {
-  // TODO: handle better choosing file to import
+  // TODO: choosing which file to import could be done better. Also cointracking paid account has API that we could use instead
   const data = readFileSync("./import/list.csv", "utf-8").replace(/^\uFEFF/, "");
   const csv = Papa.parse(data, {
     encoding: "utf-8",
@@ -33,10 +34,11 @@ export const coinTrackingImport = (): ILedgerEntry[] => {
 
 const parseData = (transactions: any[]): ILedgerEntry[] => {
   const ret: ILedgerEntry[] = [];
-  for (const transaction of transactions.slice(1)) {
+  for (const transaction of transactions.slice(1).filter(csvDateFilter)) {
     const date = Moment(transaction[9], COIN_TRACKING_DATE_FORMAT);
     const id = transaction[8];
     let type;
+    let expenseType = null;
     let sender = transaction[7];
     let receiver = transaction[7];
 
@@ -56,7 +58,6 @@ const parseData = (transactions: any[]): ILedgerEntry[] => {
     if (transaction[0] === "Deposit") {
       type = TxType.DEPOSIT;
       sender = "Unknown";
-
       senderCurrency = receiverCurrency;
       senderAmount = receiverAmount;
     } else if (transaction[0] === "Withdrawal") {
@@ -65,6 +66,7 @@ const parseData = (transactions: any[]): ILedgerEntry[] => {
       senderAmount = senderAmount.minus(feeAmount);
       receiverCurrency = senderCurrency;
       receiverAmount = senderAmount;
+      expenseType = ExpenseType.PAYMENT;
     } else if (transaction[0] === "Trade") {
       type = TxType.LOCAL;
     } else {
@@ -80,39 +82,25 @@ const parseData = (transactions: any[]): ILedgerEntry[] => {
       receiver,
       receiverCurrency,
       receiverAmount,
-      feeCurrency,
-      feeAmount,
       type,
+      expenseType,
     };
 
-    if (type === TxType.LOCAL) {
-      if (!feeAmount.isZero()) {
-        ret.push({
-          ...ledgerEntry,
-          senderAmount: new BigNumber(0),
-          receiverAmount: new BigNumber(0),
-          type: TxType.EXPENSE,
-        });
-      }
+    ret.push({
+      ...ledgerEntry,
+    });
+
+    if (!feeAmount.isZero()) {
       ret.push({
         ...ledgerEntry,
-        feeAmount: new BigNumber(0),
+        senderCurrency: feeCurrency,
+        senderAmount: feeAmount,
+        receiverCurrency: feeCurrency,
+        receiverAmount: feeAmount,
+        receiver: "Kraken exchange",
+        type: TxType.EXPENSE,
+        expenseType: ExpenseType.FEE,
       });
-    } else if (type === TxType.DEPOSIT) {
-      if (!ledgerEntry.feeAmount.isZero()) {
-        ret.push({
-          ...ledgerEntry,
-          senderAmount: new BigNumber(0),
-          receiverAmount: new BigNumber(0),
-          type: TxType.EXPENSE,
-        });
-      }
-      ret.push({
-        ...ledgerEntry,
-        feeAmount: new BigNumber(0),
-      });
-    } else {
-      ret.push(ledgerEntry);
     }
   }
 
@@ -124,23 +112,23 @@ export const combineCoinTrackingInfo = (
   coinTrackingData: ILedgerEntry[]
 ): ILedgerEntry[] => {
   const ret = ledgerData.concat(
-    coinTrackingData.filter(ledgerEntry => ledgerEntry.type === TxType.LOCAL)
+    coinTrackingData.filter(
+      ledgerEntry =>
+        ledgerEntry.type === TxType.LOCAL ||
+        (ledgerEntry.type === TxType.EXPENSE && ledgerEntry.expenseType === ExpenseType.FEE)
+    )
   );
 
   for (const coinTrackingEntry of coinTrackingData.filter(
-    ledgerEntry => ledgerEntry.type !== TxType.LOCAL
+    cte => cte.type === TxType.EXPENSE && cte.expenseType === ExpenseType.PAYMENT
   )) {
-    const matchedTxs = ret.filter(ledgerEntry => {
-      if (
-        ledgerEntry.senderAmount.eq(coinTrackingEntry.senderAmount) &&
-        ledgerEntry.senderCurrency === coinTrackingEntry.senderCurrency &&
-        ((ledgerEntry.type === TxType.EXPENSE && coinTrackingEntry.type === TxType.DEPOSIT) ||
-          (ledgerEntry.type === TxType.DEPOSIT && coinTrackingEntry.type === TxType.EXPENSE)) &&
-        ledgerEntry.date.diff(coinTrackingEntry.date, "days", true) <= 2
-      ) {
-        return true;
-      }
-    });
+    const matchedTxs = ret.filter(
+      ledgerEntry =>
+        ledgerEntry.receiverAmount.eq(coinTrackingEntry.senderAmount) &&
+        ledgerEntry.receiverCurrency === coinTrackingEntry.senderCurrency &&
+        ledgerEntry.type === TxType.DEPOSIT &&
+        Math.abs(ledgerEntry.date.diff(coinTrackingEntry.date, "days", true)) <= 1
+    );
 
     if (matchedTxs.length > 1) {
       console.log(coinTrackingEntry);
@@ -151,24 +139,56 @@ export const combineCoinTrackingInfo = (
     } else if (matchedTxs.length === 1) {
       const matchedTx = matchedTxs[0];
 
-      if (matchedTx.type === TxType.DEPOSIT) {
-        matchedTx.sender = coinTrackingEntry.sender;
-      } else {
-        matchedTx.receiver = coinTrackingEntry.sender;
-      }
+      matchedTx.sender = coinTrackingEntry.sender;
+      matchedTx.type = TxType.LOCAL;
+      matchedTx.date = coinTrackingEntry.date;
+      matchedTx.notes =
+        matchedTx.notes + `eth id: ${matchedTx.id}; cointracking id: ${coinTrackingEntry.id}`;
+      matchedTx.id = coinTrackingEntry.id;
+    } else {
+      ret.push(coinTrackingEntry);
+    }
+  }
 
-      ret.push({
-        ...matchedTx,
-        type: TxType.EXPENSE,
-        senderAmount: new BigNumber(0),
-        receiverAmount: new BigNumber(0),
-        feeAmount:
-          matchedTx.type === TxType.DEPOSIT ? coinTrackingEntry.feeAmount : matchedTx.feeAmount,
-        feeCurrency: coinTrackingEntry.feeCurrency,
-      });
+  for (const coinTrackingEntry of coinTrackingData.filter(cte => cte.type === TxType.DEPOSIT)) {
+    const matchedTxs = ret.filter(
+      ledgerEntry =>
+        ledgerEntry.senderAmount.greaterThan(coinTrackingEntry.receiverAmount.times(0.995)) &&
+        ledgerEntry.senderAmount.lessThan(coinTrackingEntry.receiverAmount.times(1.005)) &&
+        ledgerEntry.senderCurrency === coinTrackingEntry.receiverCurrency &&
+        ledgerEntry.senderCurrency === CryptoCurrency.ETH &&
+        ledgerEntry.type === TxType.EXPENSE &&
+        Math.abs(ledgerEntry.date.diff(coinTrackingEntry.date, "days", true)) <= 1
+    );
+
+    if (matchedTxs.length > 1) {
+      console.log(coinTrackingEntry);
+      console.log(matchedTxs);
+
+      throw new Error(
+        "Cointracking import error. During tx matching there is unambiguous transaction"
+      );
+    } else if (matchedTxs.length === 1) {
+      const matchedTx = matchedTxs[0];
+
+      const krakenFee = matchedTx.senderAmount.minus(coinTrackingEntry.receiverAmount);
 
       matchedTx.type = TxType.LOCAL;
-      matchedTx.feeAmount = new BigNumber(0);
+      matchedTx.receiver = coinTrackingEntry.receiver;
+      matchedTx.senderAmount = coinTrackingEntry.receiverAmount;
+      matchedTx.receiverAmount = coinTrackingEntry.receiverAmount;
+
+      const txFee = ret.filter(le => le.id === matchedTx.id && le.expenseType === ExpenseType.FEE);
+      if (txFee.length !== 1) {
+        throw new Error("Cointracking import error. During DEPOSIT fee tx matching failed. ");
+      }
+
+      txFee[0].notes =
+        txFee[0].notes +
+        `eth fee: ${txFee[0].senderAmount.toString()}; kraken fee: ${krakenFee.toString()}`;
+      txFee[0].senderAmount = txFee[0].senderAmount.plus(krakenFee);
+      txFee[0].receiverAmount = txFee[0].receiverAmount.plus(krakenFee);
+      txFee[0].receiver = txFee[0].receiver + " and Kraken exchange";
     } else {
       ret.push(coinTrackingEntry);
     }
@@ -179,10 +199,23 @@ export const combineCoinTrackingInfo = (
   return ret;
 };
 
+const csvDateFilter = (line: string[]): boolean =>
+  Moment(line[9], COIN_TRACKING_DATE_FORMAT).isBetween(config.startDate, config.endDate);
+
 const ledgerEntryComparator = (a: ILedgerEntry, b: ILedgerEntry): number => {
   const dateDiff = a.date.diff(b.date);
   if (dateDiff !== 0) {
     return dateDiff;
   }
-  return a.id.localeCompare(b.id);
+
+  const stringDiff = a.id.localeCompare(b.id);
+  if (stringDiff !== 0) {
+    return stringDiff;
+  }
+  const typeDiff = a.type.localeCompare(b.type);
+  if (typeDiff !== 0) {
+    return typeDiff;
+  }
+
+  return a.expenseType.localeCompare(b.expenseType);
 };
